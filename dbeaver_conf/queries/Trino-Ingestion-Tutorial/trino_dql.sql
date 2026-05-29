0abf43602bdf5f66fe60498a7bcc7a82fc2620a4
Egor Tarasenko <egor@ponder.co>
2026-05-30 00:59:44 +0300

    FEAT: Add Trino DDL

--- dbeaver_conf/queries/Trino-Ingestion-Tutorial/trino_dql.sql
+++ dbeaver_conf/queries/Trino-Ingestion-Tutorial/trino_dql.sql
@@ -4,4 +4,15 @@ select * from postgres.dev_test.maria_db_ingestion_customers;
 
 describe postgres.dev_test.maria_db_ingestion_customers;
 
-
+create table "postgres"."dev_test_data_ingestion"."ingestion_customers"  
+      
+    as (
+      select  customer_id,
+        first_name,
+        last_name,
+        email,
+        phone_number,
+        join_date, 
+        1/4 as "b"
+from "mariadb"."db"."customers"
+    );
